9c42643e26c3a18682134b004c88e8c7a4230587
Tama <tamara.pereira@outlook.com>
2023-11-10 22:21:19 -0300

    final

--- condicion.js
+++ condicion.js
@@ -1,12 +1,18 @@
 let hora = prompt("¿Que hora es en tu pais?");
 
-if (hora>= 6 && hora<= 12) {
+if (hora >= 6 && hora <= 12) {
     alert("Buen dia! Gracias por visitarnos");
 }
-else if (hora>= 13 && hora<= 18) {
+else if (hora >= 13 && hora <= 18) {
     alert("Buenas tardes! Gracias por visitarnos");
 }
 
 else {
     alert("Buenas noches! Gracias por visitarnos");
 }
+
+const button = document.querySelector(".btn");
+button.onclick = function () {
+    console.log("eliminar");
+};
+
